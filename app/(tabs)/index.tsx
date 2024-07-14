10e39c851851d2dbd731df86964e8b856287438a
Justine Dakiss <justinedaquis28@gmail.com>
2024-07-14 15:23:22 +0800

    created clinic card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import { StyleSheet, ScrollView, Button, Pressable, Modal, StatusBar, Dimensions, BackHandler} from 'react-native';
+import { StyleSheet, ScrollView, Button, Pressable, Modal, StatusBar, Dimensions, SafeAreaView, Image} from 'react-native';
 import {useState} from 'react';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import Box from "../../components/box";
+import Clinic from '@/components/clinicCard';
 
 export default function TabOneScreen() {
   const windowWidth= Dimensions.get("window").width;
   const windowHeight = Dimensions.get("window").height;
 
 
+  const alvarezClinic = {
+    imageSource: require("../../assets/images/clinic_image.jpg"),
+    clinicName: "Alvarez Medical Clinic",
+    specialistsCount: 4,
+    patientsCount: 84,
+    rating: 5.0,
+    locationDistance: 12
+  }
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   // false by default
@@ -41,183 +50,197 @@ export default function TabOneScreen() {
   });
   ``
   return (
-    
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={[styles.container, {padding: 20, display: "flex", flexDirection: "column", rowGap: 20}]}>
+{/*         
+        <StatusBar backgroundColor='green' barStyle="dark-content" ></StatusBar>
 
-      <StatusBar backgroundColor='green' barStyle="dark-content" hidden></StatusBar>
-             
 
-      <ScrollView style={styles.topScrollView}>
-      
-      <View style={styles.container}>
-        <Box style={{backgroundColor: "green"}}>{"hey"}</Box>
-        <Box style={{backgroundColor: "red"}}>Box 1</Box>
-        <Box style={{backgroundColor: "blue"}}>Box 1</Box>
-        <Box style={{backgroundColor: "green"}}>Box 1</Box>
-      </View>   
-      <View style={styles.container}>
-        
-       <Text>
-        📘 Courses - https://learn.codevolution.dev/
+        <ScrollView style={styles.topScrollView}>
 
-        💖 Support UPI - https://support.codevolution.dev/
-        💖 Support Paypal - https://www.paypal.me/Codevolution
-        💾 Github - https://github.com/gopinav
+          <View style={styles.container}>
+            <Box style={{ backgroundColor: "green" }}>{"hey"}</Box>
+            <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+            <Box style={{ backgroundColor: "blue" }}>Box 1</Box>
+            <Box style={{ backgroundColor: "green" }}>Box 1</Box>
+          </View>
+          <View style={styles.container}>
+            <Text>
+              📘 Courses - https://learn.codevolution.dev/
 
-        📱 Follow Codevolution
-        + Twitter -   / codevolutionweb
-        + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-        📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-        Scroll View
-        React Native Tutorial
-        React Native Tutorial for Beginners
-          📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners
+              📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners
-          📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners
+              📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners
-          📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners
+              📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners
-          📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners
+              📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
+              📫 Business - codevolution.business@gmail.com
 
-          💖 Support UPI - https://support.codevolution.dev/
-          💖 Support Paypal - https://www.paypal.me/Codevolution
-          💾 Github - https://github.com/gopinav
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners        📘 Courses - https://learn.codevolution.dev/
 
-          📱 Follow Codevolution
-          + Twitter -   / codevolutionweb
-          + Facebook -   / codevolutionweb
+              💖 Support UPI - https://support.codevolution.dev/
+              💖 Support Paypal - https://www.paypal.me/Codevolution
+              💾 Github - https://github.com/gopinav
 
-          📫 Business - codevolution.business@gmail.com
+              📱 Follow Codevolution
+              + Twitter -   / codevolutionweb
+              + Facebook -   / codevolutionweb
 
-          Scroll View
-          React Native Tutorial
-          React Native Tutorial for Beginners
-       </Text>
+              📫 Business - codevolution.business@gmail.com
 
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-       <EditScreenInfo path="app/(tabs)/index.tsx" />
+              Scroll View
+              React Native Tutorial
+              React Native Tutorial for Beginners
+            </Text>
+
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+          </View>
+
+        </ScrollView>
+        <Button title='example' color="midnightblue" onPress={() => console.log("buttonpress")}></Button>
+        <Pressable onLongPress={() => console.log("pressed!")} style={{ height: 100, width: 100, backgroundColor: 'red' }} >
+        </Pressable>
+
+        <Button onPress={() => setIsModalVisible(true)} title='example2'></Button>
+
+
+
+        <Modal
+          visible={isModalVisible}
+          onRequestClose={() => setIsModalVisible(false)}
+          animationType='slide'
+          transparent={true}
+        >
+          <View style={{ height: 300, width: 300, position: "relative", alignSelf: "center" }}>
+            <Text>Modal Content</Text>
+            <Button onPress={() => setIsModalVisible(false)} title='Close Modal'></Button>
+          </View>
+
+        </Modal> */}
+
+
+
+
+
+
+        <Clinic {...alvarezClinic}></Clinic>
+        <Clinic {...alvarezClinic}></Clinic>
+        <Clinic {...alvarezClinic}></Clinic>
+        <Clinic {...alvarezClinic}></Clinic>
+        <Clinic {...alvarezClinic}></Clinic>
+        <Clinic {...alvarezClinic}></Clinic>
 
       </View>
-      
-      </ScrollView>
-      <Button title='example' color="midnightblue" onPress={() => console.log("buttonpress")}></Button> 
-      <Pressable onLongPress={() => console.log("pressed!")} style={{height: 100, width: 100, backgroundColor: 'red'}} >
-      </Pressable>
+    </ScrollView>
 
-      <Button onPress={() => setIsModalVisible(true)}title='example2'></Button>
-      
-      
-
-      <Modal 
-      visible={isModalVisible} 
-      onRequestClose={()=> setIsModalVisible(false)}
-      animationType='slide'
-      transparent={true}
-      >
-        <View style={{height: 300, width: 300, position: "relative", alignSelf:"center"} }>
-          <Text>Modal Content</Text>
-          <Button onPress={() => setIsModalVisible(false)}title='Close Modal'></Button>
-        </View>
-
-      </Modal>
-    </View>
 
     
   );
---
+++ components/clinicCard.tsx
@@ -0,0 +1,138 @@
+import { Modal, View, Text, StyleSheet, ImageBackground, Dimensions, Image, Pressable} from "react-native";
+import { useState } from "react";
+import { withDecay } from "react-native-reanimated";
+
+
+export default function Clinic({
+    imageSource,
+    clinicName,
+    specialistsCount,
+    patientsCount,
+    rating,
+    locationDistance
+}) {
+    const [clinicModalVisible, setClinicModalVisible] = useState(false);
+
+    return (
+        
+        <Pressable onPress={() => setClinicModalVisible(true)} style={[styles.cardContainer, styles.boxShadow]}>
+            <View style={[styles.flexContainerRow, {columnGap: 30}]}>
+                <View style={[styles.flexContainerRow, {alignContent: "center", justifyContent: "center"}]}>
+                    <Image source={imageSource} style={styles.clinicImage}>
+
+                    </Image>
+
+                </View>
+
+                <View style={[styles.flexContainerColumn, {rowGap: 20, paddingTop: 10, paddingRight: 10, paddingBottom: 10}]}>
+                    <Text style={{fontSize: 28}}>{clinicName}</Text>
+
+                    <View style={{flex: 1}}>
+                        <View style={[styles.flexContainerRow, {columnGap: 10}]}>
+                            <View style={[styles.miniInfoTab, {flexGrow: 1.5}]}>
+                                {/* mini-info-section-big */}
+                                <Text style={styles.infoTabText}>{specialistsCount} Specialists</Text>
+                            </View>
+                            {/* flex row */}
+                            <View style={styles.miniInfoTab}>
+                                {/* mini-info-section-small */}
+                                <Text style={styles.infoTabText}>{patientsCount} Patients</Text>
+                            </View>
+                        </View>
+                        <View style={[styles.flexContainerRow, {columnGap: 10, marginTop: 5}]}>
+                            <View style={styles.miniInfoTab}>
+                                <Text style={styles.infoTabText}>{rating} Stars</Text>
+                                {/* mini-info-section-small */}
+                            </View>
+                            {/* flex row */}
+                            <View style={[styles.miniInfoTab, { flexGrow: 1.5}]}>
+                                <Text style={styles.infoTabText}>{locationDistance} KM away</Text>
+                                {/* mini-info-section-big */}
+                            </View>
+                        </View>
+                        <View style={{flex: 1}}>
+                            <Text style={{marginTop: "auto",alignSelf: "flex-end",fontWeight: "bold", fontSize: 12}}>Learn More</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <Modal 
+            visible={clinicModalVisible}
+            onRequestClose={() => setClinicModalVisible(false)}
+            animationType='slide'
+            >
+                <View style={{padding: 20, display: "flex", justifyContent: "center", alignItems: "center", flex: 1}}>
+                    <Text style={{fontSize: 30, fontWeight: "bold"}}>Test Info</Text> 
+                    
+                </View>
+            </Modal>
+            
+        </Pressable>
+        
+    )
+}
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height; 
+const styles = StyleSheet.create({
+    cardContainer: {
+        height: 240,
+        backgroundColor: "whitesmoke",
+        width: "80%",
+        borderRadius: 10,
+        padding: 10
+        
+        
+    },
+
+    clinicImage: {
+        flex: 1,
+        borderRadius: 3,
+        
+        height: "100%",
+        width: "100%",
+    },
+    miniInfoTab: {
+        height: 30,
+        flex: 1,
+        backgroundColor: 'rgba(31, 159, 162, 0.19)',
+        borderRadius: 7,
+        
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+            
+        
+    },
+    clinicTitle: {
+        
+    },
+    infoTabText: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#1F9FA2',
+        
+    },
+    flexContainerRow: {
+       display: 'flex',
+       flexDirection: 'row',
+       flex: 1,
+       
+    },
+    flexContainerColumn: {
+       flexDirection: 'column',
+       flex: 1,
+        
+    },
+    boxShadow: {
+        shadowColor: '#000',
+        elevation: 10,
+        
+    }
+
+
+    
+
+
+    
+})
